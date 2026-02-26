55dcc6df7f5036a9c719b714166981da8e0af532
Kirankamble12 <kirank6550@gmail.com>
2026-02-26 23:22:06 +0530

    fadeing and movement

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,28 @@
-
+import { useRef } from 'react'
+import { useGSAP } from '@gsap/react';
+import gsap from 'gsap';
 import './App.css'
 
-function App() {
-  
 
+
+function App() {
+      
+ const headlineRef = useRef();
+
+ useGSAP(() =>{
+     gsap.from(headlineRef.current,{
+      y: 30,          
+      opacity: 0,     
+      duration: 1,    
+      ease: "power3.out" 
+     })
+ })
   return (
     <>
-      <div>
-         <h1 class="text-3xl font-bold underline">
-       welcome Itzfizz
+      <div className='h-screen bg-zinc-950 flex items-center justify-center'>
+         <h1  ref={headlineRef}
+         className="text-8xl font-bold text-white tracking-tight">
+            welcome traveler
            </h1>
       </div>
       
