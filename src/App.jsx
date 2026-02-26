174dc7f6ee82bb37ca893e008cb5e86c29f527c2
Kirankamble12 <kirank6550@gmail.com>
2026-02-26 17:59:42 +0530

    setup:tailwind and the gsap

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+      <div>
+         <h1 class="text-3xl font-bold underline">
+       welcome Itzfizz
+           </h1>
+      </div>
+      
+    </>
+  )
+}
+
+export default App
